08a6ccf3b99b0f89851f27539f6b5cf23cbf6968
Gitea <gitea@fake.local>
2020-11-14 22:58:40 +0530

    project init

---
+++ docusaurus.config.js
@@ -0,0 +1,82 @@
+module.exports = {
+  title: 'Agri Supply Chain Sri Lanka',
+  tagline: 'Deep analysis on Sri Lankan agri supply chain',
+  url: 'https://Agri-supply-chain-SriLanka.copper-plus.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'copper-plus', // Usually your GitHub org/user name.
+  projectName: 'agri-supply-chain-srilanka', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: '| Agri Supply Chain Sri Lanka',
+      logo: {
+        alt: 'copper-plus',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {to: 'blog', label: 'Blog', position: 'left',activeBasePath:'blog'},
+        {
+          href: 'https://copper-plus.com/',
+          //activeBasePath: 'docs',
+          label: 'Copper-plus project',
+          position: 'left',
+        }
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Facebook',
+              href: 'https://facebook.com/copper-plus.srilanka',
+            },
+            {
+              label: 'Twitter',
+              href: '#',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/copper-plus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Legator Solutions, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'Agriculture into Next level',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        The art and science of cultivating the soil, 
+        growing crops and raising livestock. It includes the preparation
+         of plant and animal products for people to use and their distribution
+          to markets. Agriculture is the backbone of country.
+      </>
+    ),
+  },
+  {
+    title: 'Supply chain analysis',
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    description: (
+      <>
+        Data driven suply chain analysis can make a significant impact to bring 
+        more open, sustainable and fair market for all the stakeholders. We are working
+        on this approach.
+      </>
+    ),
+  },
+  {
+    title: 'Powered by Copper-plus',
+    imageUrl: 'img/logo.svg',
+    description: (
+      <>
+        Copper-plus project come up with the vision of Open, Connected and decentralized 
+        social network based commodity market for Sri Lanka. 
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('blog/')}>
+              Read More..
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
