747e971278bcbf890ffa280bea117d4e1852d054
Gitea <gitea@fake.local>
2020-11-15 19:41:04 +0530

    welcome blog post added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,13 +65,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/copper-plus/Agri-supply-chain-SriLanka/tree/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/copper-plus/Agri-supply-chain-SriLanka/tree/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
